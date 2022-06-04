4480b0db255c1b840b2ee5f0e99a2ab9af1f0c10
YT330113 <1728702316@qq.com>
2022-06-04 20:38:24 +0800

    20000604

--- MTO_TFS.C
+++ MTO_TFS.C
@@ -2,9 +2,9 @@
 
 static char help[] = "topology optimization of thermal-fluid-structural problem\n";
 #include "fvCFD.H"
-#include "singlePhaseTransportModel.H"
+#include "singlePhaseTransportModel.H"//用于粘度模型的单向输运模型，用于不可压单向流
 #include "turbulentTransportModel.H"
-#include "simpleControl.H"
+#include "simpleControl.H"//simple算法的相关控制，用于计算过程中的收敛性判断
 #include "fvOptions.H"//
 #include <MMA.h>
 #include <diff.c>
@@ -21,7 +21,7 @@ int main(int argc, char *argv[])
     #include "readTransportProperties.H" //一些物理场的声明
     #include "readThermalProperties.H" //一些物理场的声明
     #include "readMechanicalProperties.H" //一些物理场的声明
-     #include "initContinuityErrs.H"
+     #include "initContinuityErrs.H"// 用于声明与初始化连续性误差，初始误差为0
     #include "opt_initialization.H"//一些参数的声明和初始化
     while (simple.loop(runTime))
     {
